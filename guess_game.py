416caf64a0b38cf8686cb84c54d5a4df68e95e65
americo <americojr@gmail.com>
2018-09-03 23:58:28 -0300

    Programa informa feedback sobre advinhação

--- guess_game.py
+++ guess_game.py
@@ -10,15 +10,17 @@ g = input("Guess the number: ") #A função input exibirá a mensagem entre aspa
 guess = int(g) #Como a função input por default retorna uma string, transformamos a informação de "g" em um número inteiro e atribuímos a variável "guess".
 
 #A linha acima poderia ser removida se colocássemos um int() antes do input.
+# Se nós não transformarmos "g" em int o computador vai entender a entrada como uma letra. Dessa forma, mesmo que fosse digitado "5", o computador diria que você perder. Por que? Por que ele entende que se duas partes de informação são de "tipos" diferentes, não há nenhum mode de serem iguais.
 
 if guess == 5: #Faz uma comparação entre o conteúdo da variável "guess" e o número 5. Uma condição de um desvio tem o valor True ou False.
 
 #Caminho True
 	print("You win!")
-
 else:
 #Caminho False
-
-	print("You lose!")
+        if guess > 5:
+                print("Too high")
+        else:
+                print("Too low")
 
 print("Game Over!")
