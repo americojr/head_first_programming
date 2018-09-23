c71ddedbf7ac0926d331a16b26a8d75cfb2e179b
americo <americojr@gmail.com>
2018-09-23 12:51:00 -0300

    Chamada da função get_price()

---
+++ cap3/starbuzz5.py
@@ -0,0 +1,20 @@
+#Usando funções para reutilizar o código
+
+import urllib.request
+
+def get_price():
+    
+    page = urllib.request.urlopen("http://www.beans-r-us.appspot.com/prices.html")
+
+    text = page.read().decode("utf8")
+
+    where = text.find(">$")
+
+    start_of_price = where + 2
+
+    end_of_price = start_of_price + 4
+
+    print(float(text[start_of_price:end_of_price]))
+
+
+get_price()
