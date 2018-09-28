a39da722007a2950a43277647e22284e95b88e8c
americo <americojr@gmail.com>
2018-09-28 07:13:20 -0700

    update

---
+++ cap4/high_score.py
@@ -0,0 +1,18 @@
+#Exercício capítulo 4 - manipulando dados em arquivos e arrays
+
+highest_score = 0
+
+result_f = open("results.txt")
+
+for line in result_f:
+
+    if float(line) > highest_score:
+
+        highest_score = float(line)
+
+result_f.close()
+
+print("The highest score was:")
+
+print(highest_score)
+    
---
+++ cap4/split_example.py
@@ -0,0 +1,13 @@
+#Exemplo do método split disponível
+
+rock_band = "Al Carl Mike Brian"
+
+a = rock_band.split()
+
+print(a)
+
+(rhythm, lead, vocals, bass) = rock_band.split()
+
+print(rhythm, lead, vocals, bass)
+
+print(rock_band)
