dbef1948d4a527ac82e06d31a87b22ee961ccb0a
americojr <americojr@gmail.com>
2019-01-21 22:04:51 -0300

    update

---
+++ cap6/starbuzz.py
@@ -0,0 +1,7 @@
+#Módulo de Desconto Starbuzz Oficial
+#Copyright(c) Starbuzz Corporation
+#Todos os Direitos Reservados
+#Esta função calcula um desconto de 5% em um preço def discount(price)
+
+def discount(price):
+    return 0.95 * price
